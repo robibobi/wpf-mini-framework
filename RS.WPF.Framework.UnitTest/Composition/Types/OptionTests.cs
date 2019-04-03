3c7f0a96b61a25a4d186b6584e3d1aee9d58513f
Tobias Höfer <robibobi@gmx.de>
2019-04-03 22:45:37 +0200

    * Option type refactoring and first unit tests.

---
+++ RS.WPF.Framework.UnitTest/Composition/Types/OptionTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RS.WPF.Framework.Composition.Types;
+using Shouldly;
+using System;
+
+namespace RS.WPF.Framework.UnitTest.Composition.Types
+{
+    [TestClass]
+    public class OptionTests
+    {
+        [TestClass]
+        public class ObjectExtensionTests
+        {
+            [TestMethod]
+            public void AsOption_ValueNotNull_ReturnsSome()
+            {
+                string value = "Hello";
+
+                value.AsOption().IsSome.ShouldBeTrue();
+            }
+
+            [TestMethod]
+            public void AsOption_ValueIsNull_ReturnsNone()
+            {
+                string value = null;
+
+                value.AsOption().IsSome.ShouldBeFalse();
+            }
+
+            [TestMethod]
+            public void AsOption_ValueIs5_ReturnsSomeWithValue5()
+            {
+                Option<int> five = 5.AsOption();
+
+                ((Some<int>)five).Value.ShouldBe(5);
+            }
+        }
+
+        [TestClass]
+        public class OptionStaticTests
+        {
+            // Option.Some
+
+            [TestMethod]
+            public void Some_ValueNotNull_ReturnsSome()
+            {
+                Option<string> option = Option.Some("Hello");
+
+                option.IsSome.ShouldBeTrue();
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void Some_ValueIsNull_ThrowsArgumentNullException()
+            {
+                Option<string> option = Option.Some<string>(null);
+
+                option.IsSome.ShouldBeFalse();
+            }
+
+            [TestMethod]
+            public void Some_ValueIs5_ReturnsSomeWithValue5()
+            {
+                Option<int> five = Option.Some(5);
+
+                ((Some<int>)five).Value.ShouldBe(5);
+            }
+
+            // Option.None
+
+            [TestMethod]
+            public void None_ReturnsOptionNone()
+            {
+                Option<string> option = Option.None<string>();
+
+                option.IsSome.ShouldBeFalse();
+            }
+        }
+
+        [TestClass]
+        public class OptionExtensionTests
+        {
+            [TestMethod]
+            public void Map_OnSomeInt_ReturnsSomeString()
+            {
+                Option<int> intOption = Option.Some(5);
+                Option<string> strOption = intOption.Map(nr => nr.ToString());
+
+                strOption.IsSome.ShouldBeTrue();
+            }
+
+            [TestMethod]
+            public void Map_OnNoneInt_ReturnsNoneString()
+            {
+                Option<int> intOption = Option.None<int>();
+                Option<string> strOption = intOption.Map(nr => nr.ToString());
+
+                strOption.IsSome.ShouldBeFalse();
+            }
+        }
+
+    }
+}
--- RS.WPF.Framework/Composition/Types/Option.cs
+++ RS.WPF.Framework/Composition/Types/Option.cs
@@ -1,51 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RS.WPF.Framework.Composition.Types
 {
-    public abstract class Option
+
+    public static class ObjectExtensions
     {
-        public static Some<T> Some<T>(T value)
+        /// <summary>
+        /// Wraps the given value as Option<typeparamref name="T"/>.
+        /// If the value is Nullm the underlying type is
+        /// None<typeparamref name="T"/> otherwise Some<typeparamref name="T"/>
+        /// </summary>
+        public static Option<T> AsOption<T>(this T value)
         {
-            return new Some<T>(value);
+            if (value == null)
+                return Option.None<T>();
+            return Option.Some(value);
         }
-
-        public static None None => None.Instance;
-
-        public void Match<T>(Action none, Action<T> some)
-        {
-            if (this is Some<T> thisAsSome)
-                some(thisAsSome.Vlaue);
-            else
-                none();
-        }
-
-        public bool IsSome => !(this is None);
-    }
-
-    public class Some<T> : Option
-    {
-        T _value;
-        public Some(T value)
-        {
-            _value = value;
-        }
-
-        public T Vlaue => _value;
-    }
-
-    public class None : Option
-    {
-        private static None _instance = new None();
-        public static None Instance => _instance; 
     }
 
     public static class OptionExtensions
     {
 
+        public static TResult Match<TOption, TResult>(this Option<TOption> option,
+            Func<TOption, TResult> someF, TResult none)
+        {
+            if (option is Some<TOption> some)
+                return someF(some.Value);
+            else
+                return none;
+        }
+
+        public static Option<B> Map<A, B>(this Option<A> optionA, Func<A, B> func)
+        {
+            if (optionA is Some<A> someA)
+                return Option.From(func(someA.Value));
+            else
+                return Option.None<B>();
+        }
+
+        
     }
+
+    public static class Option
+    {
+        public static Option<T> Some<T>(T value)
+        {
+            if(value is null)
+                throw new ArgumentNullException(nameof(value));
+            return new Some<T>(value);
+        }
+
+        public static Option<T> None<T>()
+        {
+            return new None<T>();
+        }
+
+        public static Option<T> From<T>(T value)
+        {
+            if (value != null)
+                return new Some<T>(value);
+            else
+                return new None<T>();
+        }
+    }
+
+
+    public interface Option<T>
+    {
+        bool IsSome { get; }
+    }
+
+
+    public struct Some<T> : Option<T>
+    {
+        public T Value { get; }
+
+        public bool IsSome => true;
+
+        public Some(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            Value = value;
+        }
+    }
+
+    public struct None<T> : Option<T>
+    {
+        public bool IsSome => false;
+    }
+
 
 }
