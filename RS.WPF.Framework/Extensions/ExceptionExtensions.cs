e98c2d3cb43224184e4be82d7707c4ff75e59fcd
Tobias Höfer <robibobi@gmx.de>
2019-08-10 13:29:38 +0200

    Added extension method to throw exception object on the application dispathcer: ExceptionExtensions.ThrowOnDispatcher

---
+++ RS.WPF.Framework/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Windows;
+
+namespace RS.WPF.Framework.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static void ThrowOnDispatcher(this Exception exc)
+        {
+            Application.Current.Dispatcher.BeginInvoke(new Action(() => {
+                ExceptionDispatchInfo.Capture(exc).Throw();
+            }));
+        }
+    }
+}
